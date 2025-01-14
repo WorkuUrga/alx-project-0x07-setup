aa3e9a9bb32e71c20e9645c6d68cc0316ce506ce
WorkuUrga <urgaworku@gmail.com>
2025-01-14 14:43:29 +0300

    HookMastery: Unleashing the Power of Hooks

--- alx-project-0x07/pages/index.tsx
+++ alx-project-0x07/pages/index.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
+
+
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
 
   const handleGenerateImage = async () => {
     console.log("Generating Images");
@@ -11,7 +21,10 @@ const Home: React.FC = () => {
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
-        <p className="text-lg text-gray-700 mb-4">Generate stunning images based on your prompts!</p>
+        <p className="text-lg text-gray-700 mb-4">
+          Generate stunning images based on your prompts!
+        </p>
+
         <div className="w-full max-w-md">
           <input
             type="text"
@@ -24,9 +37,14 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
+            {/* {
+              isLoading ? "Loading..." : "Generate Image"
+            } */}
             Generate Image
           </button>
         </div>
+
+        {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
     </div>
   );
